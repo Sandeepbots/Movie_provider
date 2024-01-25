58a637857a2c69cc965af6dddac6381125bbf35b
Sandeepbots <157455755+Sandeepbots@users.noreply.github.com>
2024-01-25 13:14:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 rm -rf boted;
-git clone https://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@github.com/TGthings/Jerry boted;
+git clone https://github.com/Sandeepbots/Movie_provider /Movie_provider
 cd boted;
 pip3 install -U -r requirements.txt;
 echo "Bot Started....";
