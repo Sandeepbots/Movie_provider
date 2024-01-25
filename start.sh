40b1ab615ac260af7cc07c35d2a5464b5029ea07
Sandeepbots <157455755+Sandeepbots@users.noreply.github.com>
2024-01-25 13:26:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-rm -rf boted;
-git clone https://github.com/Sandeepbots/Movie_provider /Movie_provider
-cd boted;
-pip3 install -U -r requirements.txt;
-echo "Bot Started....";
-python3 bot.py;
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/VJBots/AutoFilterAdvance /AutoFilterAdvance
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /AutoFilterAdvance
+fi
+cd /AutoFilterAdvance
+pip3 install -U -r requirements.txt
+echo "Bot Started...."
+python3 bot.py
